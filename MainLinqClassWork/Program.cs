213cf1b2ad7243e21403dbe2760d68d4f97d9df7
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2023-01-24 17:38:23 +0200

    inputs!

--- MainLinqClassWork/Program.cs
+++ MainLinqClassWork/Program.cs
@@ -7,38 +7,45 @@ using System.Runtime.InteropServices;
 
 class Program
 {
-
     static public void Main(String[] args)
     {
-        DistinctAndAcendingTest();
-        AverageCharCountTest();
-        PrintStringStartWithCharTest();
-        StudentsTest();
+       // DistinctAndAscending distinctAndAscending = new DistinctAndAscending();
+      //  distinctAndAscending.MakeListDistinctAndAscending();
 
-    }
+       // SentanceAverage sentanceAverage = new SentanceAverage();
+       // sentanceAverage.GiveMeAverage();
 
-    public static void DistinctAndAcendingTest()
-    {
-        DistinctAndAscending distinctAndAscending = new DistinctAndAscending();
-        distinctAndAscending.Testing();
-    }
-
-    public static void AverageCharCountTest()
-    {
-        SentanceAverage senAverage = new SentanceAverage();
-        senAverage.Testing();
-    }
-
-    public static void PrintStringStartWithCharTest()
-    {
         PrintAllStringsWithThatChar printStringWithChar = new PrintAllStringsWithThatChar();
-        printStringWithChar.Testing();
-    }
-   public static void StudentsTest()
-    {
+        printStringWithChar.PrintWordsThatStartWithThatLetters();
+
         StudentAndGrade studentsAndGrades = new StudentAndGrade();
-        studentsAndGrades.Testing();
+
+        List<Student> students = new List<Student>
+        {
+        new Student { FirstName = "Daniel", ID = 101 }, // annoymous object
+         new Student { FirstName = "Amit", ID = 0 }, // annoymous object,
+          new Student { FirstName = "Roee", ID = 206697450 } // annoymous object
+        };
+
+        List<Grade> grades = new List<Grade>
+        {
+            new Grade { ID = 101, grade = 99 }, // annoymous object
+            new Grade { ID = 0, grade = 1 }, // annoymous object,
+            new Grade { ID = 206697450, grade = 110 } // annoymous object
+        };
+        studentsAndGrades.CombineStudnetAndGradeList(students, grades);
+
+       
+
     }
+
+
+
+
+
+   
+       
+
 }
 
 
@@ -54,7 +61,17 @@ public class DistinctAndAscending
    public void MakeListDistinctAndAscending()
    {
        Console.WriteLine("Please give me a list of numbers seperated by space, i will order them by ascending");
-       var distinctAscendingCollection = _listInt1.OrderBy(l => l).Distinct().ToList();
+        string userString = Console.ReadLine();
+        
+        List<string> userList = new (userString.Split(' '));
+        List<int> listInt = new List<int>();
+        foreach (string item in userList)
+        {
+            int UserNum = int.Parse(item);
+            listInt.Add(UserNum);
+        }
+
+        var distinctAscendingCollection = listInt.OrderBy(l => l).Distinct().ToList();
        foreach (var number in distinctAscendingCollection)
        {
            Console.WriteLine(number);
@@ -64,28 +81,33 @@ public class DistinctAndAscending
 
 public class SentanceAverage
 {
-    private string sAvegrage = "i am going to the mall today you are assholes";
-    private List<string> listString1 = new List<string>();
-
-   public void Testing()
+    
+   public void GiveMeAverage()
     {
-        Console.WriteLine(sAvegrage.Split(" ").ToList().Average(s => s.Length));
+        Console.WriteLine("Write me a sentance and i'll give you the average word count");
+       string inputString = Console.ReadLine();
+        Console.WriteLine(inputString.Split(" ").ToList().Average(s => s.Length));
 
     }
 }
 
 public class PrintAllStringsWithThatChar
 {
-    char a = 'A';
-    char b = 'B';
-    private List<string> sList = new List<string>()
+   public void PrintWordsThatStartWithThatLetters()
     {
-        "Hello", "Mom" ,"Crack", "Phone", "About", "Banana"
-    };
-
-   public void Testing()
-    {
-        List<string> listAB = sList.Where(s => s.StartsWith(a) || s.StartsWith(b)).ToList();
+        Console.WriteLine("write me a sentance and later i print the words that start with letters of your choosing");
+        List<string> sentance = Console.ReadLine().Split(' ').ToList();
+        Console.WriteLine("give me two letters and i'll give you all the words that start with them, seperate them by space");
+        List<string> letterList = Console.ReadLine().Split(' ').ToList();
+        List<char> charLetterList = new List<char>();
+        foreach (string letter in letterList)
+        {
+            char letterChar = char.Parse(letter);
+            charLetterList.Add(letterChar);
+        }
+        char a = charLetterList[0];
+        char b = charLetterList[1];
+        List<string> listAB =  sentance.Where(s => s.StartsWith(a) || s.StartsWith(b)).ToList();
         foreach (var item in listAB)
         {
             Console.WriteLine(item);
@@ -95,23 +117,8 @@ public class PrintAllStringsWithThatChar
 
 public class StudentAndGrade
 {
-    public void Testing()
+    public void CombineStudnetAndGradeList(List<Student> students, List<Grade> grades)
     {
-       
-        List<Student> students = new List<Student>
-        {
-        new Student { FirstName = "Daniel", ID = 101 }, // annoymous object
-         new Student { FirstName = "Amit", ID = 0 }, // annoymous object,
-          new Student { FirstName = "Roee", ID = 206697450 } // annoymous object
-        };
-
-        List<Grade> grades = new List<Grade>
-        {
-            new Grade { ID = 101, grade = 99 }, // annoymous object
-            new Grade { ID = 0, grade = 1 }, // annoymous object,
-            new Grade { ID = 206697450, grade = 110 } // annoymous object
-        };
-
         var studentGrades = students.Join(grades, (s) => s.ID, (g) => g.ID,
             (s, g) => new { Name = s.FirstName , grade = g.grade });
 
@@ -119,8 +126,6 @@ public class StudentAndGrade
         {
             Console.WriteLine(item);
         }
-
-
     }
 }
 
